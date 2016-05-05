cd896d46b9a6749e3b73c7721b2f8ab8306ff320
z01 <z01@z01.com>
2016-05-05 16:53:35 +0300

    AT-1079: Watcher check was corrected

--- src/app/scripts/services/service.js
+++ src/app/scripts/services/service.js
@@ -75,7 +75,7 @@
             function watch(key, func, deep) {
 
                 $rootScope.$watch(function () { return $localStorage[key]; },function(newVal, oldVal) {
-                    if(oldVal !== undefined) {
+                    if(newVal !== undefined) {
                         func(newVal, oldVal);
                     }
                 },deep)
