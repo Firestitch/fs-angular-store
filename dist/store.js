ab7af6cc1d238c7a8e7c31897407274421518fcf
z01 <z01@z01.com>
2016-05-05 17:04:15 +0300

    AT-1079: Watcher check was corrected

--- dist/store.js
+++ dist/store.js
@@ -77,7 +77,7 @@
             function watch(key, func, deep) {
 
                 $rootScope.$watch(function () { return $localStorage[key]; },function(newVal, oldVal) {
-                    if(newVal  !== undefined) {
+                    if(newVal !== undefined) {
                         func(newVal, oldVal);
                     }
                 },deep)
